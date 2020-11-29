75790d8279c86e7cd2834989a286707c7b560ac7
sahara <performa532+git@gmail.com>
2020-11-29 15:41:50 +0900

    add xcode project

---
+++ Combine/combine-print-sample/combine-print-sample/combine_print_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  combine_print_sampleApp.swift
+//  combine-print-sample
+//
+//  Created by ogasawara on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct combine_print_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
